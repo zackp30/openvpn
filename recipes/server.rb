8850e5b8ad4773e5a3c95554373db6b6021b5bdf
Zack Piper <zack.piper123@gmail.com>
2016-02-20 23:28:40 +0100

    The commit message was empty.

--- recipes/server.rb
+++ recipes/server.rb
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'openvpn::enable_ip_forwarding'
 include_recipe 'openvpn::install_bridge_utils' if node['openvpn']['type'] == 'bridge'
 include_recipe 'openvpn::install'
 
